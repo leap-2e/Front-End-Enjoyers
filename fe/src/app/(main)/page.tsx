3be4d859eaab24939d844d307820c7a04ad52a80
24LP6042 24LP6042 <24LP6042@G303-CC0138.local>
2025-04-22 13:45:45 +0800

    up

--- fe/src/app/(main)/page.tsx
+++ fe/src/app/(main)/page.tsx
@@ -1,6 +1,5 @@
 import { CreatePaymentInfo } from "../_components/CreatePaymentInfo";
 import { CreateProfile } from "../_components/CreateProfile";
-import DonationScreenCreator from "../_components/DonationScreenCreator";
 import { Header } from "../_components/Header";
 
 export default function Home() {
