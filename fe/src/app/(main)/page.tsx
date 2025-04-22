5b9b7e626d86334acfe1ec9aa29bec8c256b0118
ayagul-ma <65903730+ayagul-ma@users.noreply.github.com>
2025-04-23 00:42:31 +0800

    dashboard

--- fe/src/app/(main)/page.tsx
+++ fe/src/app/(main)/page.tsx
@@ -1,11 +1,13 @@
 import { CreatePaymentInfo } from "../_components/CreatePaymentInfo";
 import { CreateProfile } from "../_components/CreateProfile";
+import { Dashboard } from "../_components/Dashboard";
 
 export default function Home() {
   return (
     <div>
-      <CreateProfile />
-      <CreatePaymentInfo />
+      {/* <CreateProfile />
+      <CreatePaymentInfo /> */}
+      <Dashboard></Dashboard>
     </div>
   );
 }
