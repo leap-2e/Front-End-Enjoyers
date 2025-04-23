6cfc7eb3588f570962e858e5bfbc690f8ef63eb8
24LP3420 24LP3420 <24LP3420@G303-CC0142.local>
2025-04-23 10:20:48 +0800

    be post

--- be/src/db.ts
+++ be/src/db.ts
@@ -1,15 +1,11 @@
 import postgres from "postgres";
 import dotenv from "dotenv"
+import { neon } from '@neondatabase/serverless'
 
 dotenv.config();
-const DATA_BASE = process.env.DATA_BASE;
 
-const sql = postgres({
-    host: 'localhost',
-    port: 5432,
-    database: DATA_BASE,
-    // username: "Buy-Me-Coffee_owner",
-    // password: "npg_GdJKmNSQ51OW",
-})
+const DATA_BASE = process.env.DATA_BASE || '';
 
-export default sql
+export const sql = neon(DATA_BASE);
+
+
--- be/src/index.ts
+++ be/src/index.ts
@@ -1,13 +1,23 @@
 import express from "express";
-import sql from "./db";
+import { sql } from "./db";
 
 const app = express();
 app.use(express.json());
 
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
     const result = await sql`SELECT NOW() AS time`;
     res.send(`Hello from Express + PostgreSQL! Server time: ${result[0].time}`);
 });
+app.post('/', async (req, res) => {
+    const { name, about } = req.body
+    const newProfile = await sql`INSERT INTO profiles (name, about)
+    VALUES (${name}, ${about})
+    `
+    res.json({ success: true, newProfile })
+    console.log("success true")
+})
+
+
 
 const PORT = 8080;
 app.listen(PORT, () => {
