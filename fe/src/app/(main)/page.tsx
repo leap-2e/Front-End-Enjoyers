799badc0ef7ce735d31699b3a3970726829a106b
ZaBye1 <turbat639@gmail.com>
2025-04-23 10:43:05 +0800

    bug

--- fe/src/app/(main)/page.tsx
+++ fe/src/app/(main)/page.tsx
@@ -1,6 +1,5 @@
 import { CreatePaymentInfo } from "../_components/CreatePaymentInfo";
 import { CreateProfile } from "../_components/CreateProfile";
-import DonationScreenCreator from "../_components/DonationScreenCreator";
 import Explore from "../_components/Explore";
 import { Header } from "../_components/Header";
 
