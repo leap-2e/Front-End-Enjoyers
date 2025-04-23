48616f9b149ec60869bb78a0d3f88aad8e905762
ZaBye1 <turbat639@gmail.com>
2025-04-23 10:35:27 +0800

    explore

--- fe/src/app/(main)/page.tsx
+++ fe/src/app/(main)/page.tsx
@@ -1,6 +1,7 @@
 import { CreatePaymentInfo } from "../_components/CreatePaymentInfo";
 import { CreateProfile } from "../_components/CreateProfile";
 import DonationScreenCreator from "../_components/DonationScreenCreator";
+import Explore from "../_components/Explore";
 import { Header } from "../_components/Header";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <CreateProfile />
       <CreatePaymentInfo />
+      <Explore />
     </div>
   );
 }
