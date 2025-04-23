5987bfbe14ba65a38818639ea419d9c5d28ff5f5
ider-17 <ider7037@gmail.com>
2025-04-23 22:20:09 +0800

    edit profile button added dialog

--- fe/src/app/_components/BuyCoffee.tsx
+++ fe/src/app/_components/BuyCoffee.tsx
@@ -1,29 +1,71 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { Coffee } from "lucide-react";
+import { useForm } from "react-hook-form";
 
 export function BuyCoffee() {
+    const form = useForm()
+
     return (
         <div className="w-1/2 h-fit rounded-lg bg-white flex flex-col gap-8 border border-[#F4F4F5] p-6">
 
-            <div className="w-full h-[122px]">
-                <h1 className="w-[full] h-[36px] text-black font-bold mb-6">Buy Jake a Coffee</h1>
-                <div className="w-full h-[62px]">
-                    <p>Select amount:</p>
-                    <div className="h-[40px] flex gap-3">
-                        <button className="bg-[#F4F4F5] rounded-md flex gap-2 items-center py-2 px-4">
-                            <Coffee /> $1
-                        </button>
-                        <button className="bg-[#F4F4F5] rounded-md flex gap-2 items-center py-2 px-4">
-                            <Coffee /> $2
-                        </button>
-                        <button className="bg-[#F4F4F5] rounded-md flex gap-2 items-center py-2 px-4">
-                            <Coffee /> $5
-                        </button>
-                        <button className="bg-[#F4F4F5] rounded-md flex gap-2 items-center py-2 px-4">
-                            <Coffee /> $10
-                        </button>
+            <Form {...form}>
+                <form className="flex flex-col gap-8">
+                    <div className="w-full h-[122px]">
+                        <h1 className="w-[full] h-[36px] text-black font-bold mb-6">Buy Jake a Coffee</h1>
+                        <div className="w-full h-[62px]">
+                            <p>Select amount:</p>
+                            <div className="h-[40px] flex gap-3">
+                                <button className="bg-[#F4F4F5] rounded-md flex gap-2 items-center py-2 px-4">
+                                    <Coffee /> $1
+                                </button>
+                                <button className="bg-[#F4F4F5] rounded-md flex gap-2 items-center py-2 px-4">
+                                    <Coffee /> $2
+                                </button>
+                                <button className="bg-[#F4F4F5] rounded-md flex gap-2 items-center py-2 px-4">
+                                    <Coffee /> $5
+                                </button>
+                                <button className="bg-[#F4F4F5] rounded-md flex gap-2 items-center py-2 px-4">
+                                    <Coffee /> $10
+                                </button>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Enter BuyMeCoffee or social acount URL:</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="buymeacoffee.com/" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Special message:</FormLabel>
+                                <FormControl>
+                                    <textarea placeholder="Please write your message here" className="h-[131px] border rounded-md py-2 px-3"></textarea>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <Button>Support</Button>
+                </form>
+            </Form>
         </div>
     )
 }
--- fe/src/app/_components/EditProfile.tsx
+++ fe/src/app/_components/EditProfile.tsx
@@ -1,17 +1,24 @@
+"use client";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Separator } from "@/components/ui/separator";
 import { Heart } from "lucide-react";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
 import {
     Dialog,
     DialogContent,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
-} from "@/components/ui/dialog"
-import { CreateProfile } from "./CreateProfile";
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 
 export function EditProfile() {
+    const form = useForm();
+
     return (
         <div className="w-1/2 h-fit rounded-lg bg-white flex flex-col gap-5 justify-self-center">
             <div className="w-full h-[233px] p-6 gap-2 border border-[#F4F4F5] rounded-md">
@@ -28,14 +35,71 @@ export function EditProfile() {
                         <DialogTrigger>
                             <div className="bg-[#F4F4F5] py-2 px-3 rounded-md hover:bg-black hover:text-white">Edit page</div>
                         </DialogTrigger>
-                        <DialogContent className="w-2/4 h-3/5">
-                            <DialogHeader>
+                        <DialogContent className="w-3/5 h-fit flex flex-col gap-7">
+                            <DialogHeader className="h-fit">
                                 <DialogTitle>Edit profile</DialogTitle>
                                 <p className="text-[#71717A]">Make changes to your profile here. Click save when you're done.</p>
                             </DialogHeader>
-                            <div>
-                                <CreateProfile />
-                            </div>
+
+                            <Form {...form}>
+                                <form className="flex flex-col gap-6">
+                                    <div>Add photo</div>
+                                    <Avatar className="w-[160px] h-[160px]">
+                                        <AvatarImage src="https://github.com/shadcn.png" />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+
+
+                                    <div className="flex flex-col gap-3">
+                                        <FormField
+                                            control={form.control}
+                                            name="name"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>name</FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="Enter your name..." {...field} />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+
+                                        <FormField
+                                            control={form.control}
+                                            name="username"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>About</FormLabel>
+                                                    <FormControl>
+                                                        <textarea placeholder="Please write your message here" className="h-[131px] border rounded-md py-2 px-3"></textarea>
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+
+                                        <FormField
+                                            control={form.control}
+                                            name="name"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>Social media URL</FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="Enter your social account..." {...field} />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                    </div>
+
+                                    <div className="flex gap-3 justify-end items-center">
+                                        <button className="bg-[#F4F4F5] py-[6px] px-4 rounded-md">Cancel</button>
+                                        <Button className="py-2 px-4">Save changes</Button>
+                                    </div>
+                                </form>
+                            </Form>
                         </DialogContent>
                     </Dialog>
 
