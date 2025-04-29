a496cecf526ea78e0da1fffe442618238938a1f6
Narangerel0121 <nrngrl0121@gmail.com>
2025-04-29 13:47:29 +0800

    filter error fixed

--- fe/src/app/_components/SideBar.tsx
+++ fe/src/app/_components/SideBar.tsx
@@ -7,78 +7,80 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 type UserType = {
-  username: string;
-  id: string;
+    username: string;
+    id: string;
 };
 
 export function SideBarComponent() {
-  const [userId, setUserId] = useState("");
-  const [userName, setUserName] = useState<string>("");
+    const [userId, setUserId] = useState("");
+    const [userName, setUserName] = useState<string>("");
 
-  useEffect(() => {
-    const username = localStorage.getItem("username") as string;
-    setUserName(username);
-  }, []);
+    useEffect(() => {
+        const username = localStorage.getItem("username") as string;
+        setUserName(username);
+    }, []);
 
-  const getId = async () => {
-    const { data: users } = await axios.get(`${BASE_URL}/users`);
-    // const {data: users} = await axios.get(`${BASE_URL}/users?userName=${userName}`); // search query
+    const getId = async () => {
+        const { data: users } = await axios.get(`${BASE_URL}/users`);
+        // const {data: users} = await axios.get(`${BASE_URL}/users?userName=${userName}`); // search query
 
-    const filteredUsers: UserType[] = users.filter((user: UserType) => {
-      if (userName === user.username) {
-        return user;
-      }
-    });
+        const getId = async () => {
+            const { data: users } = await axios.get(`${BASE_URL}/users`);
+            // const {data: users} = await axios.get(`${BASE_URL}/users?userName=${userName}`); // search query
 
-    if (filteredUsers.length) {
-      setUserId(filteredUsers[0].id);
+            const filteredUsers: UserType[] = users.users.filter((user: UserType) => {
+                if (userName === user.username) {
+                    return user
+                }
+            });
+
+            if (filteredUsers.length) {
+                setUserId(filteredUsers[0].id);
+            }
+
+            if (filteredUsers.length) {
+                setUserId(filteredUsers[0].id);
+            }
+        };
     }
-  };
-  console.log(userId, "userId");
+    console.log(userId, "userId");
 
-  useEffect(() => {
-    if (userName) {
-      getId();
-    }
-  }, [userName]);
+    useEffect(() => {
+        if (userName) {
+            getId();
+        }
+    }, [userName]);
 
-  return (
-    <div className="min-w-[260px] max-w-[300px] h-screen">
-      <nav className="mt-[100px]">
-        <ul className="space-y-2">
-          <li>
-            <Link href="/">
-              <span className="block p-2 rounded-md hover:bg-[#F4F4F5]">
-                Home
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/explore">
-              <span className="block p-2 rounded-md hover:bg-[#F4F4F5]">
-                Explore
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/view">
-              <span className="flex items-center gap-1 p-2 rounded-md hover:bg-[#F4F4F5]">
-                View page
-                <ExternalLink size={15} />
-              </span>
-            </Link>
-          </li>
-          <li>
-            {userId && (
-              <Link href={`/settings/${userId}`}>
-                <span className="block p-2 rounded hover:bg-[#F4F4F5]">
-                  Account settings
-                </span>
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+
+    return (
+        <div className="min-w-[260px] max-w-[300px] h-screen">
+            <nav className="mt-[100px]">
+                <ul className="space-y-2">
+                    <li>
+                        <Link href="/">
+                            <span className="block p-2 rounded-md hover:bg-[#F4F4F5]">Home</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/explore">
+                            <span className="block p-2 rounded-md hover:bg-[#F4F4F5]">Explore</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/view">
+                            <span className="flex items-center gap-1 p-2 rounded-md hover:bg-[#F4F4F5]">
+                                View page
+                                <ExternalLink size={15} />
+                            </span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={`/settings/${userId}`}>
+                            <span className="block p-2 rounded hover:bg-[#F4F4F5]">Account settings</span>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
 }
