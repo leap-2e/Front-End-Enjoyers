7d405496c78d7290e97d1e921681b7623ec58c0b
ider-17 <ider7037@gmail.com>
2025-05-01 09:17:51 +0800

    updated

--- fe/src/app/(main)/view/page.tsx
+++ fe/src/app/(main)/view/page.tsx
@@ -23,19 +23,24 @@ export default function ViewPage() {
     const [userId, setUserId] = useState("");
     const [file, setFile] = useState<string | File>("");
     const [currentProfile, setCurrentProfile] = useState<CreatorType>();
-    const [coverButton, setCoverButton] = useState(false);
+    const [hasCover, setHasCover] = useState(false);
+    const [isEditingCover, setIsEditingCover] = useState(false);
 
     const handleImage = (event: ChangeEvent) => {
         const file = ((event.target as HTMLInputElement).files as FileList)[0]
         setImageUrl(window.URL.createObjectURL(file))
         setFile(file);
+        setIsEditingCover(true);
     };
-    console.log(imageUrl, "imageURL")
-    console.log(file, "file")
+
+    const handleChangeImage = (event: ChangeEvent) => {
+        const file = ((event.target as HTMLInputElement).files as FileList)[0]
+        setImageUrl(window.URL.createObjectURL(file));
+        setIsEditingCover(true);
+    };
 
     const cancelButton = () => {
-        setImageUrl("");
-        setFile("");
+        setIsEditingCover(false);
     };
 
     useEffect(() => {
@@ -62,6 +67,9 @@ export default function ViewPage() {
     formData.append("upload_preset", UPLOAD_PRESET);
 
     const saveChanges = async () => {
+        setHasCover(true);
+        setIsEditingCover(false);
+
         const response = await fetch(`https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
             {
                 method: "POST",
@@ -70,12 +78,6 @@ export default function ViewPage() {
         const { url } = await response.json();
 
         const res = await axios.post(`${BASE_URL}/profiles/image`, { background_image: url, user_id: userId });
-
-        setCoverButton(true);
-    };
-
-    const changeCoverOnclick = () => {
-        setCoverButton(false)
     };
 
     return (
@@ -84,22 +86,28 @@ export default function ViewPage() {
             <Header />
 
             <div className="w-full h-[319px] flex justify-center items-center bg-cover bg-center mt-[56px] relative" style={{ backgroundImage: `url('${imageUrl ? imageUrl : "bg-[#F4F4F5]"}')` }}>
-                <div className={`bg-black rounded-md ${imageUrl ? "hidden" : "block"}`}>
-                    <Label htmlFor="background" className="py-2 px-4 bg-transparent text-white flex gap-3 cursor-pointer"><Camera />Add image</Label>
-                    <input id="background" type="file" className="hidden" onChange={handleImage} />
-                </div>
 
-                <div className={`absolute top-2 right-10 flex gap-3 ${imageUrl ? "block" : "hidden"} ${coverButton ? "hidden" : "flex"}`}>
-                    <Button onClick={saveChanges} className="bg-black text-white hover:bg-[#F4F4F5] hover:text-black">Save changes</Button>
-                    <Button onClick={cancelButton} className="bg-[#F4F4F5] text-black hover:text-white hover:bg-black">Cancel</Button>
-                </div>
+                {!hasCover && !isEditingCover && (
+                    <div className="bg-black rounded-md">
+                        <Label htmlFor="background" className="py-2 px-4 bg-transparent text-white flex gap-3 cursor-pointer"><Camera />Add image</Label>
+                        <input id="background" type="file" className="hidden" onChange={handleImage} />
+                    </div>
+                )};
 
-                {/* <Button className={`flex absolute top-2 right-10 bg-[#F4F4F5] text-black hover:bg-black hover:text-white ${coverButton ? "flex" : "hidden"}`}><Camera />Change cover</Button> */}
+                {isEditingCover && (
+                    <div className="absolute top-2 right-10 flex gap-3">
+                        <Button onClick={saveChanges} className="bg-black text-white hover:bg-[#F4F4F5] hover:text-black">Save changes</Button>
+                        <Button onClick={cancelButton} className="bg-[#F4F4F5] text-black hover:text-white hover:bg-black">Cancel</Button>
+                    </div>
+                )};
 
-                <div onClick={changeCoverOnclick} className={`flex absolute top-2 right-10 bg-[#F4F4F5] text-black hover:bg-black hover:text-white rounded-md ${coverButton ? "flex" : "hidden"}`}>
-                    <Label htmlFor="cover" className="py-2 px-3 flex gap-2"><Camera />Change cover</Label>
-                    <input id="cover" type="file" className="hidden" onChange={handleImage} />
-                </div>
+                {hasCover && !isEditingCover && (
+                    <div className="flex absolute top-2 right-10 bg-[#F4F4F5] text-black hover:bg-black hover:text-white rounded-md">
+                        <Label htmlFor="cover" className="py-2 px-3 flex gap-2"><Camera />Change cover</Label>
+                        <input id="cover" type="file" className="hidden" onChange={handleChangeImage} />
+                    </div>
+                )};
+
             </div>
 
             <div className="w-[90%] flex gap-5 mt-[-100px] mx-auto relative">
