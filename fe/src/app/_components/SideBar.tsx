181e3c10d1b3a81ac08391526ed9921d6503aac5
Narangerel0121 <nrngrl0121@gmail.com>
2025-05-01 10:15:01 +0800

    3refresh = 0 onclick

--- fe/src/app/_components/SideBar.tsx
+++ fe/src/app/_components/SideBar.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { ExternalLink } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import {jwtDecode} from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
 type DecodeType = {
     email: string,
@@ -39,7 +39,7 @@ export function SideBarComponent() {
                         </Link>
                     </li>
                     <li>
-                        <Link href="/view">
+                        <Link href={`/view?user_id=${userId}`}>
                             <span className="flex items-center gap-1 p-2 rounded-md hover:bg-[#F4F4F5]">
                                 View page
                                 <ExternalLink size={15} />
